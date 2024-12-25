daef1b31d62815c7fa931a091f9732540abd221a
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-12-25 20:04:07 +0700

    docs: improve documentation for MultiContextProvider

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,34 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 /**
- * A simple React utility for wrapping multiple context providers
+ * MultiContextProvider adalah komponen React yang memungkinkan penggunaan beberapa
+ * context providers secara bersamaan. Komponen ini menerima daftar context providers
+ * dan children yang akan dibungkus oleh context providers tersebut.
  *
- * @param {FC<{ children: ReactNode }>[]} contexts
- *        An array of context providers to wrap the children with.
- * @param {ReactNode} children
- *        The children to be wrapped with the context providers.
- * @returns {ReactElement}
- *          The wrapped children component.
+ * @component
+ * @param {Object} props - Props untuk komponen MultiContextProvider.
+ * @param {FC<{ children: ReactNode }>[] } props.contexts - Array dari context providers
+ * yang akan digunakan untuk membungkus children. Setiap context provider harus
+ * merupakan komponen fungsional yang menerima props `children`.
+ * @param {ReactNode} props.children - Elemen React yang akan dibungkus oleh context
+ * providers. Ini bisa berupa komponen, elemen JSX, atau elemen lainnya yang valid
+ * dalam React.
+ *
+ * @returns {ReactNode} - Elemen React yang merupakan hasil dari membungkus children
+ * dengan semua context providers yang diberikan.
+ *
+ * @example
+ * import React from 'react';
+ * import { UserProvider } from './User Context';
+ * import { ThemeProvider } from './ThemeContext';
+ * import MultiContextProvider from './MultiContextProvider';
+ *
+ * const App = () => {
+ *   return (
+ *     <MultiContextProvider contexts={[User Provider, ThemeProvider]}>
+ *       <YourComponent />
+ *     </MultiContextProvider>
+ *   );
+ * };
  */
 const MultiContextProvider = ({ contexts, children }) => {
     const wrapContexts = (children, contextProviders) => {
