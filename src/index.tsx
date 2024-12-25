38673dbd41c9dbb71b12fe9840473d0ccec25278
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-12-25 19:09:57 +0700

    chore(release): 1.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,16 @@ interface MultiContextProviderProps {
   children: ReactNode;
 }
 
+/**
+ * A simple React utility for wrapping multiple context providers
+ *
+ * @param {FC<{ children: ReactNode }>[]} contexts
+ *        An array of context providers to wrap the children with.
+ * @param {ReactNode} children
+ *        The children to be wrapped with the context providers.
+ * @returns {ReactElement}
+ *          The wrapped children component.
+ */
 const MultiContextProvider: React.FC<MultiContextProviderProps> = ({ contexts, children }) => {
   const wrapContexts = (children: ReactNode, contextProviders: FC<{ children: ReactNode }>[]) => {
     return contextProviders.reduceRight((acc, ContextProvider) => {
